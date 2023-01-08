8586cea6ef7600a803b43d995fb4722b4f128d3f
Stiven M <stvnm33@gmail.com>
2023-01-08 18:36:00 -0500

    Base project

--- src/index.js
+++ src/index.js
@@ -3,4 +3,9 @@ import Express from 'express';
 const app = Express();
 const port = process.env.PORT || 3000;
 
+// routes
+app.get('/',(req,res) =>{
+    res.send('Welcome to my API')
+})
+
 app.listen(port,() => console.log(`Running on port ${port}`));
