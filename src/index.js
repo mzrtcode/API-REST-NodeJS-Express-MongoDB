4c925653d5cfc127936fa5017957a7f7275039bf
Stiven M <stvnm33@gmail.com>
2023-01-08 19:22:12 -0500

    Creating user's routes

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import Express from "express";
 import Mongoose from "mongoose";
 import * as dotenv from "dotenv";
 import mongoose from "mongoose";
+import userRoutes from './routes/user.js'
 
 dotenv.config();
 const app = Express();
 const port = process.env.PORT || 3000;
+
+// middleware
+app.use('/api', userRoutes)
 
 // routes
 app.get("/", (req, res) => {
